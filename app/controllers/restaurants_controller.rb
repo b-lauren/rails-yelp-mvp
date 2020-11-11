f7a68364bcbb4d4e860a3666cbbf301fc9118962
Rebecca Ross <b.laurenross@gmail.com>
2020-11-11 16:10:18 +0000

    yelp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,9 +3,33 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
-  def show
+  def new
+    @restaurant = Restaurant.new
   end
 
-  def new
+  def create
+
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.'
+  end
+
+  private
+
+  # Only allow a trusted parameter "white list" through.
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :stars)
   end
 end
